26a9c07668b8fdac5571966e14fc030419edc44a
armanmasangkay <vss.arman@gmail.com>
2022-03-10 16:41:36 +0800

    fix breadcrumb issue

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -66,15 +66,15 @@
         <div class="container pt-3 pb-1">
             <nav aria-label="breadcrumb mb-0">
                 <ol class="breadcrumb">
-                    <li><a href="/" class="me-1">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="/" class="me-1">Dashboard</a></li>
                     @for ($x = 0 ; $x < count($datas['sections']) ; $x++)
                         @if ($datas['sections'][$x] != $datas['sections'][count($datas['sections']) - 1])
                         <li class="breadcrumb-item">
-                            <a class="ms-1" href="{{route('dashboard', ['dir' => 'public/' .$datas['urls'][$x]])}}">{{$datas['sections'][$x]}}</a>
+                            <a class="ms-1" href="{{route('dashboard', ['dir' => 'public/' .$datas['urls'][$x]])}}">{{ucwords($datas['sections'][$x])}}</a>
                         </li>
                         @else
                         <li class="breadcrumb-item">
-                            <a href="{{route('dashboard', ['dir' => 'public/' .$datas['urls'][$x]])}}" class="ms-1 text-primary">{{$datas['sections'][$x]}}</a>
+                            <a href="{{route('dashboard', ['dir' => 'public/' .$datas['urls'][$x]])}}" class="ms-1 text-primary">{{ucwords($datas['sections'][$x])}}</a>
                         </li>
                         @endif
                     @endfor
